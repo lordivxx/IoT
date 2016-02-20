96bdffc1e90b73c99b81e5c1aac4e75fb0a5fa84
Chuck Forsyth <chuck.forsyth@gmail.com>
2016-02-20 13:12:41 -0500

    cleaned up code a little

--- sub.py
+++ sub.py
@@ -17,19 +17,7 @@
 # This shows a simple example of an MQTT subscriber.
 
 import sys
-try:
-    import paho.mqtt.client as mqtt
-except ImportError:
-    # This part is only required to run the example from within the examples
-    # directory when the module itself is not installed.
-    #
-    # If you have the module installed, just use "import paho.mqtt.client"
-    import os
-    import inspect
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../src")))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-    import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
 
 def on_connect(mqttc, obj, flags, rc):
     print("rc: "+str(rc))
